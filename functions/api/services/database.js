ca2b045803856ac55f76f029be7226181b8da670
Orshi Kozek <35982478+OrshiKozek@users.noreply.github.com>
2020-11-18 15:02:07 -0800

    deleted problematic lines

--- functions/api/services/database.js
+++ functions/api/services/database.js
@@ -415,8 +415,7 @@ const searchAnimals = async (colonyId, searchCriteria) => {
 
   const snapshot = await animalsRef.get();
   const results = snapshot.docs.map(doc => doc.data());
-  const searchResults = { animals: results, colonyId };
-  return searchResults;
+  
 };
 
 module.exports = {
